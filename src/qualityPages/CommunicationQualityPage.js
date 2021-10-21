3a43dd62bda71d5e7e91dcf17f4ff7fb483d02fa
Sharodh K R <61955784+krsharodh@users.noreply.github.com>
2021-10-22 00:07:14 +1100

    communication dropdown added

--- src/qualityPages/CommunicationQualityPage.js
+++ src/qualityPages/CommunicationQualityPage.js
@@ -7,6 +7,11 @@ import Table from "../_utils/Table";
 import { alertConstants } from "../_constants";
 import { InformationalNote } from "../_utils/Alert";
 
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+
 class CommunicationPage extends React.Component {
   constructor(props) {
     super(props);
@@ -25,6 +30,12 @@ class CommunicationPage extends React.Component {
       ],
       hasConfig:
         this.props.teamInfo && this.props.teamInfo[this.props.currentTeamKey],
+      sprintSelected: "Meeting+notes",
+      sprintList: [
+        { label: "All", value: "Meeting+notes" },
+        { label: "Sprint 1", value: "Sprint+1" },
+        { label: "Sprint 2", value: "Sprint+2" }
+      ]
     };
   }
 
@@ -52,12 +63,33 @@ class CommunicationPage extends React.Component {
                   message={alertConstants.NO_MEETING_MINUTES}
                 />
               )}
+            {
+              <div style={{ marginLeft: "2rem" }}>
+                <FormControl fullWidth >
+                  <InputLabel id="demo-simple-select-label">Sprint</InputLabel>
+                  <Select
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    value={this.state.selectedSprint}
+                    label="Sprint"
+                    onChange={(e) => {
+
+                      this.setState({ selectedSprint: e.target.value })
+                    }}
+                  >
+                    {this.state.sprintList.map(el => <MenuItem value={el.value}>{el.label}</MenuItem>)}
+                  </Select>
+                </FormControl>
+              </div>
+            }
             {this.state.hasConfig &&
               this.props.confluenceData &&
               this.props.confluenceData.length != 0 && (
                 <Table
                   columns={this.state.columns}
-                  data={this.props.confluenceData}
+                  data={this.props.confluenceData.filter(el => {
+                    return el.link.includes(this.state.selectedSprint)
+                  })}
                   width={"80vw"}
                   height={"50vh"}
                 />
